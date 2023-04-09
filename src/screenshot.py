6c4441aa1ad7187f07a023efbe025458583d95dc
Takakusagi Kazushi <kusagi@TakakusaginoMacBook-Pro.local>
2023-04-09 10:43:51 +0900

    スクリーンショットを撮る

--- src/screenshot.py
+++ src/screenshot.py
@@ -1,3 +1,11 @@
 """
 screencapture ./images/$(date "+%y%m%d_%H%M%S")_image.png
+# Select Display ID
+screencapture -l <display_id> screen.png
+
 """
+
+import os
+
+os.makedirs('images', exist_ok=True)
+os.system('screencapture ./images/$(date "+%y%m%d_%H%M%S")_image.png')
