a832e6312be26a8688433376b6697c8b52b9c23e
髙草木 和史 <kazushi.takakusagi@gmail.com>
2023-04-11 11:14:52 +0900

    screenshot method

--- src/screenshot.py
+++ src/screenshot.py
@@ -7,5 +7,6 @@ screencapture -l <display_id> screen.png
 
 import os
 
-os.makedirs('images', exist_ok=True)
-os.system('screencapture ./images/$(date "+%y%m%d_%H%M%S")_image.png')
+def screenshot():
+    os.makedirs('images', exist_ok=True)
+    os.system('screencapture ./images/$(date "+%y%m%d_%H%M%S")_image.png')
