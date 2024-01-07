6746e183e82d1e7857710b419147c828ad3b6504
Takakusagi Kazushi <kusagi@TakakusaginoMacBook-Pro.local>
2024-01-07 15:29:56 +0900

    configの宣言はmainのみ

--- src/logger.py
+++
@@ -1,8 +0,0 @@
-import json
-from logging import getLogger, config
-
-with open("./log_config.json", "r") as f:
-    log_conf = json.load(f)
-
-config.dictConfig(log_conf)
-# logger = getLogger("app")
